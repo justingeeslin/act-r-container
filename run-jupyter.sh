b7e36882857303aa380ab2fa4e5520f7e6ee9dd6
Justin Geeslin <justin@geesl.in>
2024-09-18 14:32:47 -0500

    Notebooks that don’t hold up the deploy

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -4,4 +4,22 @@ set -m
 
 cd ${HOME}
  
-jupyter notebook  --NotebookApp.default_url="/notebooks/tutorial.ipynb" --ip=0.0.0.0 --port=8888 --NotebookApp.trust_xheaders=True --NotebookApp.allow_origin=*
+# Start the Jupyter Notebook server in the background
+jupyter notebook --NotebookApp.default_url="/notebooks/tutorial.ipynb" --ip=0.0.0.0 --port=8888 --NotebookApp.trust_xheaders=True --NotebookApp.allow_origin=* &
+
+# Capture the PID of the background process
+JUPYTER_PID=$!
+
+# Give the server some time to start
+sleep 10
+
+# Check if the Jupyter Notebook server is running
+if ps -p $JUPYTER_PID > /dev/null
+then
+   echo "Jupyter Notebook server started successfully."
+else
+   echo "Failed to start Jupyter Notebook server."
+fi
+
+# Optionally, you can wait for the server to finish running, or you can keep the script running
+# wait $JUPYTER_PID
